670498e12911416c286897aedb87e7d46ccf99b3
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-28 08:39:48 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  return [...array, element]
+}
+
+
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
